a7983aa7714a536a007edf5a1912dc501792ade7
koteitan <ukaibutton@mac.com>
2019-06-01 00:51:49 -0400

    parse sheet and make text.

--- sheet2js/main.js
+++ sheet2js/main.js
@@ -1,3 +1,29 @@
+var Entry=function(line){
+  var col = line.content.$t.split(",");
+  this.name  = col[0].split(": ")[1];
+  this.year  = col[1].split(": ")[1];
+  this.order = col[2].split(": ")[1];
+}
+Entry.prototype.toString = function(){
+  return this.year + ":" + this.name + "(" + this.order + ")";
+}
+var expand=function(res){
+  var sheet = res.feed.entry;
+  var entries = sheet.length;
+  var entrylist = [];
+  for(var e=0;e<entries;e++){
+    var entry = new Entry(sheet[e]);
+    entrylist.push(entry);
+  }
+
+  var str="";
+  for(var e=0;e<entries;e++){
+    str = str + entrylist[e].toString();
+    str = str + "\n";
+  }
+  document.getElementById("debug").innerHTML = str;
+}
+
 var entry=function(){
   var spreadsheetId = "11WH6PrhFAcdMEWSTjxSjZ7_rWHg-b8shAvSFn99bdyQ",
     url = "https://spreadsheets.google.com/feeds/list/" +
@@ -6,7 +32,7 @@ var entry=function(){
   $.get({
     url: url,
     success: function(response) {
-      console.log(response);
+      expand(response);
     }
   });
 };
