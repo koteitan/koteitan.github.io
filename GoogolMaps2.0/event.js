ed8e393f3ecdfd9fe93a49a9721f96d3008dff5b
koteitan <ukaibutton@mac.com>
2019-06-16 23:21:59 -0400

    initEvent() is moved into event.js/initEvent() uses parameter/addcomments for event.js/bugfix of empty cells

--- GoogolMaps2.0/event.js
+++ GoogolMaps2.0/event.js
@@ -2,6 +2,7 @@
   event.js
   GUI event control. queueing the 
   events and process in serial order.
+  See Appendix for detail.
 ----------------------------------*/
 //events ----------------------
 //variables
@@ -15,14 +16,41 @@ var isShiftKey = false;
 var mouseDownPos = [-1,-1];
 var mousePos     = [-1,-1];
 var mouseUpPos   = [-1,-1];
-// procedure
-var removeClientOffset = function(e){
-  if(e.target.getBoundingClientRect){
-    var rect = e.target.getBoundingClientRect();
-    return [e.x-rect.left, e.y-rect.top];
-  }
-  return [e.x, e.y];
+var isKeyTyping;
+//init
+var initEvent = function(can){
+  eventQueue = [];
+  can.ontouchstart = addTouchEvent;
+  can.ontouchmove  = addTouchEvent;
+  can.ontouchend   = addTouchEvent;
+  can.onmousedown  = addEvent;
+  can.onmousemove  = addEvent;
+  can.onmouseup    = addEvent;
+  can.onmouseout   = addEvent;
+  can.onmousewheel = addEvent;
+//  window.onkeydown       = addEvent;
 };
+// addEvent(Event e)
+var addEvent = function(e){
+  if(e.type=="mousewheel"){
+    var a=1;
+  }
+  if(eventQueue.length < eventsMax && e!=undefined){
+    eventQueue.push(e);
+    lastEvent = e;//for debug
+  }
+  if(e.type!="keydown" || e.type=="mousewheel"){
+    if(!isKeyTyping && isMouseOver){
+      if(e.preventDefault) e.preventDefault();
+      e.returnValue = false;
+    }
+  }
+};
+var addTouchEvent = function(){
+  event.preventDefault();
+  eventQueue.push(event);
+}
+// process in game loop
 var procEvent = function(){
   while(eventQueue.length>0){
     var e = eventQueue.shift(); // <MouseEvent>
@@ -91,24 +119,60 @@ var procEvent = function(){
     }
   }
 };
-// sub routines
-// addEvent(Event e)
-var addEvent = function(e){
-  if(e.type=="mousewheel"){
-    var a=1;
+var removeClientOffset = function(e){
+  if(e.target.getBoundingClientRect){
+    var rect = e.target.getBoundingClientRect();
+    return [e.x-rect.left, e.y-rect.top];
   }
-  if(eventQueue.length < eventsMax && e!=undefined){
-    eventQueue.push(e);
-    lastEvent = e;//for debug
-  }
-  if(e.type!="keydown" || e.type=="mousewheel"){
-    if(!isKeyTyping && isMouseOver){
-      if(e.preventDefault) e.preventDefault();
-      e.returnValue = false;
-    }
-  }
+  return [e.x, e.y];
 };
-var addTouchEvent = function(){
-  event.preventDefault();
-  eventQueue.push(event);
-}
+/*--------------------------------------------------------
+  Appendix:
+
+    Usage:
+
+      Using event.js, user can easily add the mouse/touch events doing below:
+      (1) Include event.js in HTML.
+      (2) Call initEvent(can) before your game loop begins. 
+          The parameter can is your canvas object.
+      (3) Call procEvent() in your game loop.
+      (4) Write handlers you like.
+      That's all!
+
+    Handlers references:
+
+      Handler functions:
+        handleMouseDown()     = Getting mouse button pressing. (or tap)
+        handleMouseUp()       = Getting mouse button releasing (or exit to outside of GUI).
+        handleMouseDragging() = Getting mouse dragging between press and release. (or swipe)
+        handleMouseMove()     = Getting mouse moving without press.
+        handleKeyDown()       = Getting key down
+        handleMouseWheel()    = Getting mouse wheel motion.
+      variables:
+        mouseDownPos[d] = mouse position in d th dimension (d=0 is x, d=1 is y)
+        mousePos[d]     = mouse position (handleMouse{Dragging, Mode, Wheel})
+        mouseWheel[d]   = mouse wheel rotation mount in d th dimension. unit is unknown.
+
+    How it works in details:
+      (1) initEvent() adds event listeners of your canvas.
+      (2) When an event occurs, the event listener addEvent() or addTouchEvent() is
+          called from another process.
+      (3) The event listeners add the event into eventQueue and quit this process.
+      (4) procEvent() in your game loop is watching eventQueue and call a proper handler.
+      (5) Your handler works.
+      That's all!
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
