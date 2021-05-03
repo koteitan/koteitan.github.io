d1ccb982b409b121daec90b3f68d4bbe81e7d418
koteitan <ukaibutton@mac.com>
2021-05-04 05:59:19 +0900

    v1.0./Jansen's link works after bufgix

---
+++ linksim/version.js
@@ -0,0 +1,8 @@
+document.getElementById("versiondisplay").innerHTML = "1.0";
+document.getElementById("changelog").innerHTML = 
+"<table align='right'><tr>"+
+"<td style='text-align:right'>2021/04/25 20:00 JST</td><td>&nbsp;</td><td>started coding.</td></tr>"+
+"<td style='text-align:right'>2021/05/04 06:00 JST</td><td>&nbsp;</td><td>Jansen's linkage works.</td></tr>"+
+"</table>";
+document.getElementById("knownbugs").innerHTML = 
+"<table align='right'><tr><td style='text-align:right'>under construction.</td></tr></table>";
